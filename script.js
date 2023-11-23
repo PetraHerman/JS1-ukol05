a062bc737d6f3a4b9375127f5cc01ffcb51ce76b
Petra Hermanová <petra.dorotikova@gmail.com>
2023-11-23 11:25:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Zadání
+// Vytvořte funkci salary se třemi parametry
+
+// wage - hrubá mzda v korunách za hodinu
+// hours - kolik hodin denně průměrně pracujete
+// days - kolik dní v měsící průměrně pracujete
+// Funkce spočítá vaši hrubou měsíční mzdu v celých korunách.
+
+// Dále vytvořte funkci taxed, která na vstupu obdrží částku a procento zdanění, a vrátí částku v celých korunách zdaněnou podle zadaných procent.
+
+// Použítím funkcí salary a taxed spočítejte svoji měsíční mzdu po 15% zdanění.
+
